8674b1f54c5fef15728192c9b763dac62332847d
Mola Pahnadayan <mola.mp@gmail.com>
2021-09-01 16:23:11 +0430

    update script

--- sign_setup.sh
+++ sign_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 mkdir -p /root/sign/
+cp sign.sh /root/sign/sign.sh
+
 cd /root/sign/
 openssl genrsa -out repo.key 2048
 chmod 0400 repo.key
 openssl rsa -in repo.key -out repo.pub -pubout
 printf "function: sha256\nfingerprint: `sha256 -q repo.pub`\n" > fingerprint
 
-cp sign.sh /root/sign/sign.sh
 chmod +x /root/sign/sign.sh
